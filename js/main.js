bdfa73b73f67760e998af0b38d82341033398f36
Alex <9970421@gmail.com>
2020-01-27 22:37:45 +0300

    исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -7,57 +7,82 @@ var MIN_X = 1;
 var MAX_X = 1200;
 var MIN_Y = 130;
 var MAX_Y = 630;
-var TYPES_RESIDENCE = ["palace", "flat", "house", "bungalo"];
-var CONVENIENCES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-var CHECK_TIMES = ["12:00", "13:00", "14:00"];
+var TYPES_RESIDENCE = ['palace', 'flat', 'house', 'bungalo'];
+var CONVENIENCES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var CHECK_TIMES = ['12:00', '13:00', '14:00'];
 var MIN_PRICE = 1;
 var MAX_PRICE = 1000000;
+var MIN_LENGTH_STRING = 1;
+var MAX_LENGTH_STRING = 100;
+var MIN_COUNT_ROOMS = 1;
+var MAX_COUNT_ROOMS = 3;
+var MIN_COUNT_GUESTS = 0;
+var MAX_COUNT_GUESTS = 3;
+var MIN_COUNT_PHOTOS = 2;
+var MAX_COUNT_PHOTOS = 5;
 
 
 var getRandom = function (elements) {
   return elements[getRandomRange(0, elements.length)];
 }
 
-var getRandomRange = function (minNum, maxNum) {
-  return Math.floor(Math.random() * (maxNum - minNum)) + minNum;
+var getRandomRange = function (minElement, maxElement) {
+  return Math.floor(Math.random() * (maxElement - minElement)) + minElement;
 }
 
 var getRandomSelection = function (elements) {
-  var currentElements = elements.slice;
+  var currentElements = elements.slice();
   var countElementsDelete = getRandomRange(0, currentElements.length);
   for (var i = 0; i < countElementsDelete; i++) {
-    var selectedNum = getRandomRange(0, currentElements.length);
-    currentElements[selectedNum] = currentElements[currentElements.length - 1];
+    var selectedElement = getRandomRange(0, currentElements.length);
+    currentElements[selectedElement] = currentElements[currentElements.length - 1];
     currentElements.length--;
   }
   return currentElements;
 }
 
+var getRandomString = function (minLength, maxLength) {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var randomString = '';
+  while (randomString.length < getRandomRange(minLength, maxLength)) {
+    randomString += alphabet[getRandomRange(0, alphabet.length)];
+  }
+  return randomString;
+}
+
+var getArrayPhotos = function (countPhotos) {
+  var elements = [];
+  for (var i = 0; i < countPhotos; i++) {
+    elements[i] = 'http://' + getRandomString(MIN_LENGTH_STRING, MAX_LENGTH_STRING) + '/hotel' + (i + 1) + '.jpg';
+  }
+  return elements;
+}
+
 var createOffers = function (count) {
   var offers = [];
-  var timeChecking = getRandom(CHECK_TIMES);
 
   for (var i = 0; i < count; i++) {
-    var adNum = i + 1;
+    var timeChecking = getRandom(CHECK_TIMES);
+    var sequenceNumber = i + 1;
     var currentX = getRandomRange(MIN_X, MAX_X);
     var currentY = getRandomRange(MIN_Y, MAX_Y);
 
     offers[i] = {
       author: {
-        avatar: "img/avatars/user0" + adNum + ".png"
+        avatar: 'img/avatars/user0' + sequenceNumber + '.png'
       },
       offer: {
-        title: "Заголовок " + adNum,
-        address: currentX + ", " + currentY,
+        title: 'Заголовок ' + sequenceNumber,
+        address: currentX + ', ' + currentY,
         price: getRandomRange(MIN_PRICE, MAX_PRICE + 1),
         type: getRandom(TYPES_RESIDENCE),
-        rooms: getRandomRange(1, 4),
-        guests: getRandomRange(0, 4),
-        checkin: "после " + timeChecking,
-        checkout: "до " + timeChecking,
+        rooms: getRandomRange(MIN_COUNT_ROOMS, MAX_COUNT_ROOMS + 1),
+        guests: getRandomRange(MIN_COUNT_GUESTS, MAX_COUNT_GUESTS + 1),
+        checkin: 'после ' + timeChecking,
+        checkout: 'до ' + timeChecking,
         features: getRandomSelection(CONVENIENCES),
-        description: "строка с описанием " + adNum,
-        photos: ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"]
+        description: 'строка с описанием ' + sequenceNumber,
+        photos: getArrayPhotos(getRandomRange(MIN_COUNT_PHOTOS, MAX_COUNT_PHOTOS + 1))
       },
       location: {
         x: currentX,
@@ -71,7 +96,7 @@ var createOffers = function (count) {
 
 var renderPin = function (element) {
   var pinElement = nearbyPinTemplate.cloneNode(true);
-  pinElement.style.cssText = "left: " + (element.location.x - PIN_OFFSET_X) + "px; top: " + (element.location.y - PIN_OFFSET_Y) + "px;";
+  pinElement.style.cssText = 'left: ' + (element.location.x - PIN_OFFSET_X) + 'px; top: ' + (element.location.y - PIN_OFFSET_Y) + 'px;';
   pinElement.querySelector('img').src = element.author.avatar;
   pinElement.querySelector('img').alt = element.offer.title;
   return pinElement;
@@ -89,4 +114,8 @@ document.querySelector('.map').classList.remove('map--faded');
 var nearbyPin = document.querySelector('.map__pins');
 var nearbyPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
+
 makePinBlock(createOffers(NEARBY_ADS));
+
+var deb = createOffers(8);
+debugger;
