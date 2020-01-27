59bfca2d8ec75cd2a8a9929fe47f35c82ae1ef5a
Alex <9970421@gmail.com>
2020-01-27 15:19:31 +0300

    добавляет генерацию меток

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var NEARBY_ADS = 8;
+var PIN_OFFSET_X = 25;
+var PIN_OFFSET_Y = 70;
 
 var getRandom = function (elements) {
   return elements[getRandomRange(0, elements.length)];
@@ -31,12 +33,13 @@ var createOffers = function (count) {
   var photoAddresses = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
 
   for (var i = 0; i < count; i++) {
+    var adNum = i + 1;
     offers[i] = {
       author: {
-        avatar: "img/avatars/user0" + (i + 1) + ".png"
+        avatar: "img/avatars/user0" + adNum + ".png"
       },
       offer: {
-        title: "Заголовок " + (i + 1),
+        title: "Заголовок " + adNum,
         address: "600, 350",
         price: getRandomRange(100, 500),
         type: getRandom(typesResidence),
@@ -45,7 +48,7 @@ var createOffers = function (count) {
         checkin: getRandom(timeChecking),
         checkout: getRandom(timeChecking),
         features: getRandomSelect(conveniences),
-        description: "строка с описанием " + (i + 1),
+        description: "строка с описанием " + adNum,
         photos: getRandom(photoAddresses)
       },
       location: {
@@ -55,6 +58,27 @@ var createOffers = function (count) {
     }
   }
   return offers;
-}
+};
 
-debugger;
+
+var renderPin = function (element) {
+  var pinElement = nearbyPinTemplate.cloneNode(true);
+  pinElement.style = "left: " + (element.location.x - PIN_OFFSET_X) + "px; top: " + (element.location.y - PIN_OFFSET_Y) + "px;";
+  pinElement.querySelector('img').src = element.author.avatar;
+  pinElement.querySelector('img').alt = element.offer.title;
+  return pinElement;
+};
+
+var makePinBlock = function (elements) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < elements.length; i++) {
+    fragment.appendChild(renderPin(elements[i]));
+  }
+  nearbyPin.appendChild(fragment);
+};
+
+document.querySelector('.map').classList.remove('map--faded');
+var nearbyPin = document.querySelector('.map__pins');
+var nearbyPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+makePinBlock(createOffers(NEARBY_ADS));
